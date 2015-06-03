3253b8eece83f20168e8ee3e0248dfe3efeb9dcd
Robert Smiles Jr <smilesr120@gmail.com>
2015-06-02 22:55:56 -0400

    working except for displaying the ordered  statistics

--- lib/commitchamp.rb
+++ lib/commitchamp.rb
@@ -19,25 +19,27 @@ module Commitchamp
       @github = Github.new
     end
 
-# ask user for token
-  # def user_pass
-  #   if ENV['OAUTH_TOKEN'].nil?
-  #     puts "What is your github token code?"
-  #   end
-  # end
-# show current repo db
-  def show_table
-    puts "TEST: SHOW TABLE"
+    def get_auth_token
+      puts "Please enter your personal access token for Github:"
+        #    /^[0-9a-f]{40}$/
+    end
+
+
+  def show_repos
+    all_repos = Commitchamp::Reposit.all
+    puts "****** Current Repos in the Database ******"
+    all_repos.each_with_index do |r,i|
+     puts "#{i + 1} - #{r.reposit_name}"
+     puts ""
+    end
   end
 
   def introduction
-    # puts "What is your github username"
-    # @owner_name = gets.chomp
-# add validation
     puts "Would you like contribution statistics for
-      (1) a repository currently in the database, or
+      (1) a repository currently in the database above, or
       (2) a new repository"
     response = gets.chomp
+     show_repos
     until response =~ /^[12]$/
       puts "Please try again."
       response = gets.chomp
@@ -45,17 +47,12 @@ module Commitchamp
     response.to_i
   end
 
-# bad input message
-
 # enter name of repo if from db
   def name_of_repo
     puts "What is the name of the repo from the database that you would like statistics for:"
       @repo_name = gets.chomp
-    # until repo_name =~ /^\w*$/
-    #   puts "Please try again."
-    #   repo_name = gets.chomp
-    # end
   end
+
 
 # enter name of org and repo if needs to be fetched
   def name_of_org
@@ -66,56 +63,48 @@ module Commitchamp
      #  response_org = gets.chomp
     # end
   end
+
 # query the table
   def query_table
-    puts @user_person.includes(:contribution).order("contribution.additions :desc")
+    puts "nothing yet"
+    #display_repos = Commitchamp::Reposit.select(:reposit_name).order(:additions + :deletions + :contribs)
   end
 
 # get contributors from selected repo
   def pull_contributors#(@owner_name, @repo_name)
-
     contributors = @github.get_contributors(@org_name, @repo_name)
-#
     contributors.each do |x|
         table_contributor(x)
     end
   end
-# temp1.each do |xx|
-#   a.push(xx[0]["weeks"][0]["a"])
-#   b.push(xx[0]["weeks"][0]["d"])
-# end
-# put contributors in db
+
   def table_contributor(contributor)
-    binding.pry
-     @user_person = User.find_or_create_by(user_name: contributor["author"]["login"])
+     @user = User.find_or_create_by(user_name: contributor["author"]["login"])
      lines_added = contributor['weeks'].map { |x| x['a'] }.sum
      lines_deleted = contributor['weeks'].map{ |x| x['d'] }.sum
      commits_made = contributor['weeks'].map { |x| x['c'] }.sum
      repo = Reposit.find_or_create_by(reposit_name: @repo_name)
-     contrib = Contribution.find_or_create_by(user_id: user.id,
+     contrib = Contribution.find_or_create_by(user_id: @user.id,
                                               additions: lines_added,
                                               deletions: lines_deleted,
-                                              changes: commits_made,
+                                              contribs: commits_made,
                                               repo_id: repo.id)
+     binding.pry
   end
 
 
   def run
     response = 0
-    # user_pass
-    show_table
+    show_repos
     response = introduction
     if response == 1
-      puts "number 1"
       name_of_repo
     elsif response == 2
-      puts "number 2"
       owner_name = name_of_org
       repo_name = name_of_repo
       pull_contributors#(owner_name, repo_name)
     end
     query_table
-    show_table
   end
  end
   binding.pry
